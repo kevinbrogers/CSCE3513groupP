936b1731b5cf0368c4007aa6529e60ff328260b7
aeramey <aeramey@uark.edu>
2014-04-07 12:01:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,63 +6,79 @@
 
 #include <iostream>
 #include "keyinput.h"
-//added includes
 #include <stdio.h>
-#include <string>  //can be changed at a different time; for containing filename
+#include <cstdlib>
 #include "chip8.h"
-
 using namespace std;
 
+// Display Size
+#define SCREEN_WIDTH 64
+#define SCREEN_HEIGHT 32
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * main.cpp DEMO VERSION                                                       *
+ * This file is the demo version of main.cpp.  It shows the functionality of   *
+ * all working parts and has comments for non-functional requirments to be     *
+ * implemented later.  This is just to prove that all the parts work.  Main.cpp*
+ * takes a filename, converts it into a character, loads the ROM, and then     *
+ * proceeds to emulate ten cycles, each of which are printed in a new array,   *
+ * to make sure that the display, input, and system itself is working correctly*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 int main() {
-    int keys[16]; // array to show which key is pressed; access to this array is necessary for opcodes EX9E, EXA1, and FX0A
-    keyinput keyInput;
-
-    // get input; this section should be included in the loop so that it runs every time the screen refreshes
-    keyInput.getInput();
-    for (int i = 0; i < 16; i++) // ensure array is full of 0's
-        keys[i] = 0;
-    if (keyInput.getLocation() != -1) // place a 1 in the proper array location
-        keys[keyInput.getLocation()] = 1;
-
-    // testing to show input is taken properly; not to be included in final program
-    cout << "your entry gives the array: ";
-    for (int i = 0; i < 16; i++)
-        cout << keys[i];
-    cout << endl;
-    
-    
-    //Amanda: Added a working array that changes from 0 to " " when drawFlag is true.  However, I'm not familiar with 
-    //        ncurses.  Since kRogers uses them for his files, I am currently giving myself a crash course in how to use
-    //        them to render this array in a window.  For now, it will reprint the array every time the array is changed.
-    //        That should be enough for a demo, but I'm still going to try to get that window working.
-    //variables
-    string filename;  //will have user select at somepoint; during testing pick 1 game to load
-    int displayx = 32;
-    int displayy= 64;
+    //instantiate a new chip8 emulator and new keyinput handler
     chip8 newChip8;
-
-
-    //set up render system with curses
+    keyinput keyInput;
     
-    //initialize chip8 system and load game into memory
-    if(!newChip8.load(filename))
+    //set up render system with ncurses
+    //Final Version only
+    
+    //Final version will allow the user to select from a number of ROMS.  For Demo,
+    //we're just using pong as an example
+    //Load ROM.  If loadROM fails to load the file, return 1
+    if(!newChip8.loadROM("Pong"))
         return 1;
-    
-    int count=0;  //FOR TESTING, delete in final version
+        
     //emulate loop
-    while(count < 2)  //FOR DISPLAY TESTING!  Change to "true" for actual game loop
+    //In the final design, while should be set to true, so the system will keep
+    //looping indefinitely, so long as the game is running.  For our Demo, we are
+    //only allowing it to loop 10 times because that should be enough to prove
+    //functionality
+    int count=0;  //REMOVE IN FINAL VERSION
+    while(count < 10)  //CHANGE TO TRUE IN FINAL VERSION
     {
     	//emulate one cycle
-        newChip8.emulate();
+        newChip8.emulateCycle();
 
 	//if draw flag is set, update screen
 	if(newChip8.drawFlag)
 		newChip8.render();
-
-        count++;
-	//store key press state
+        
+        //store key press state
 	//newChip8.setKeys();
+
+        count++;  //REMOVE IN FINAL VERSION
+        
      }
+     
+     //FOR TESTING!  //Because the display just reprints the array and does not
+                     //yet make use of ncurses for the window, the following
+                     //code tests that key inputs are working correctly.
+                     //Final version will integrate seamlessly with display
+    
+    // get input; this section should be included in the loop so that it runs every time the screen refreshes
+    keyInput.getInput();
+    for (int i = 0; i < 16; i++) // ensure array is full of 0's
+        newChip8.keys[i] = 0;
+    if (keyInput.getLocation() != -1) // place a 1 in the proper array location
+        newChip8.keys[keyInput.getLocation()] = 1;
+
+    // testing to show input is taken properly; not to be included in final program
+    cout << "your entry gives the array: ";
+    for (int i = 0; i < 16; i++)
+        cout << newChip8.keys[i];
+    cout << endl;
+
 
     return 0;
     
