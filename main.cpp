814a8c6e6eebb2e8873023e776f2e4bd7f92b43c
aeramey <aeramey@uark.edu>
2014-04-06 21:59:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@
 
 #include <iostream>
 #include "keyinput.h"
+//added includes
+#include <stdio.h>
+#include <string>  //can be changed at a different time; for containing filename
+#include "chip8.h"
 
 using namespace std;
 
@@ -25,4 +29,41 @@ int main() {
     for (int i = 0; i < 16; i++)
         cout << keys[i];
     cout << endl;
+    
+    
+    //Amanda: Added a working array that changes from 0 to " " when drawFlag is true.  However, I'm not familiar with 
+    //        ncurses.  Since kRogers uses them for his files, I am currently giving myself a crash course in how to use
+    //        them to render this array in a window.  For now, it will reprint the array every time the array is changed.
+    //        That should be enough for a demo, but I'm still going to try to get that window working.
+    //variables
+    string filename;  //will have user select at somepoint; during testing pick 1 game to load
+    int displayx = 32;
+    int displayy= 64;
+    chip8 newChip8;
+
+
+    //set up render system with curses
+    
+    //initialize chip8 system and load game into memory
+    if(!newChip8.load(filename))
+        return 1;
+    
+    int count=0;  //FOR TESTING, delete in final version
+    //emulate loop
+    while(count < 2)  //FOR DISPLAY TESTING!  Change to "true" for actual game loop
+    {
+    	//emulate one cycle
+        newChip8.emulate();
+
+	//if draw flag is set, update screen
+	if(newChip8.drawFlag)
+		newChip8.render();
+
+        count++;
+	//store key press state
+	//newChip8.setKeys();
+     }
+
+    return 0;
+    
 }
